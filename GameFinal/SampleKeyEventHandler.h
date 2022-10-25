3c94a33f0810d5ca6a5a525490e17595f83a04ec
Vu Nguyen <phihoangvu.nguyen@evizi.com>
2022-10-25 23:55:04 +0700

    Upload Base project

---
+++ GameFinal/SampleKeyEventHandler.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Scence.h"
+
+class CSampleKeyHandler: public CSceneKeyHandler
+{
+public:
+	virtual void KeyState(BYTE *states);
+	virtual void OnKeyDown(int KeyCode);
+	virtual void OnKeyUp(int KeyCode);
+	CSampleKeyHandler(LPSCENE s) :CSceneKeyHandler(s) {};
+};
