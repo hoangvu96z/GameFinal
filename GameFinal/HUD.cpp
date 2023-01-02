6db9d76b7df1a6e5196bccf9388d2b1ccd968ada
Vu Nguyen <phihoangvu.nguyen@evizi.com>
2023-01-02 23:23:12 +0700

    Update HUD and class stack speed

--- GameFinal/HUD.cpp
+++ GameFinal/HUD.cpp
@@ -1,5 +1,7 @@
 #include "HUD.h"
 
+#include "Mario.h"
+CMario* mario = CMario::GetInstance();
 HUD* HUD::__instance = NULL;
 HUD* HUD::GetInstance()
 {
@@ -10,14 +12,23 @@ HUD* HUD::GetInstance()
 
 void HUD::Render()
 {
-	float x = CGame::GetInstance()->GetCamX() + CGame::GetInstance()->GetBackBufferWidth() / 2 + SCREEN_W_PLUS;
+	float x = CGame::GetInstance()->GetCamX() + CGame::GetInstance()->GetBackBufferWidth() / 2  + SCREEN_W_PLUS;
 	float y = CGame::GetInstance()->GetCamY() + CGame::GetInstance()->GetBackBufferHeight() - SCREEN_H / 2;
 	CAnimations* animations = CAnimations::GetInstance();
-	animations->Get(ID_ANI_HUD)->Render(x, y + SCREEN_PLUS);
-	animations->Get(ID_ANI_ITEM)->Render(x + SCREEN_W, y + SCREEN_PLUS);
+	animations->Get(ID_ANI_HUD)->Render(x , y + SCREEN_PLUS);
+	animations->Get(ID_ANI_ITEM)->Render(x + SCREEN_W , y + SCREEN_PLUS);
+	stackspeed->RenderStack(CGame::GetInstance()->GetCamX() + 66 , y + 7, stack);
 }
 
 void HUD::Update(DWORD dt)
 {
 
+	//if (Range < abs(mario->GetAX()) < Range1)
+	//{
+	//	stack = 1;
+	//}
+	//else if (Range1 < abs(mario->GetAX()) < Range2)
+	//{
+	//	stack = 2;
+	//}
 }
